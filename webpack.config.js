d6afc40982a2c6c7a960222e3ae83591b563238d
Harshita <harshita.gupta@produktmacher.com>
2019-01-11 23:33:25 +0100

    added support for the browsersync and sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+const ExtractTextPlugin = require('extract-text-webpack-plugin') //
+const CleanWebpackPlugin = require('clean-webpack-plugin') // to remove/clean your build folder(s) before building
+const CopyWebpackPlugin = require('copy-webpack-plugin')  //Copies individual files or entire directories to the build directory
+const ImageminPlugin = require('imagemin-webpack-plugin').default //uses Imagemin to compress all images in your project.
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+const webpack = require('webpack')
+
+const isProduction = process.argv.indexOf('-p') !== -1
+// Use this outside because this will be referenced in loader too
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'css/main.css'
+})
+
+const plugins = [
+  extractPlugin,
+  new CleanWebpackPlugin(['dist'], {}),
+  new CleanWebpackPlugin(['images'], {}),
+  new CopyWebpackPlugin([{
+    from: __dirname + '/src/public/images',
+    to: 'images'
+  }]),
+  new ImageminPlugin({
+    disable: !isProduction, // Disable during development
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    pngquant: {
+      quality: '50-80'
+    }
+  }),
+  new BrowserSyncPlugin({
+    host: 'localhost',
+    port: 7700
+  }),
+  new HtmlWebpackPlugin({
+    template: __dirname + "/src/public/index.html",
+    inject: 'body'
+  })
+]
 
 module.exports = {
   entry: __dirname + '/src/app/index.js', // webpack entry point. Module to start building dependency graph
   output: {
-    path: __dirname + '/dist',  // Folder to store generated bundle
+    path: path.resolve(__dirname, 'dist'),  // Folder to store generated bundle
     filename: 'bundle.js',  // Name of generated bundle after build
     publicPath: '/' // public URL of the output directory when referenced in a browser
   },
@@ -18,22 +57,18 @@ module.exports = {
       },
       {
         test: /\.(sass|scss)$/,
-        use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-        }, {
+        use: extractPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
             loader: "css-loader" // translates CSS into CommonJS
-        }, {
+          }, {
             loader: "sass-loader" // compiles Sass to CSS
-        }]
+          }]
+        })
       }
     ]
   },
-  plugins: [  // Array of plugins to apply to build chunk
-    new HtmlWebpackPlugin({
-        template: __dirname + "/src/public/index.html",
-        inject: 'body'
-    })
-  ],
+  plugins,
   devServer: {  // configuration for webpack-dev-server
     contentBase: './src/public',  //source of static assets
     port: 7700, // port to run dev-server
